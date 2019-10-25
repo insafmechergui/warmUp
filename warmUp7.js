bcdb89e3be2d02a81998397c62d1572f2c369f12
insafmechergui <insaf_mechergui@outlook.com>
2019-10-25 17:45:58 +0100

    warmpup

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,29 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+var firstName = "Ons";
+var lastName = "Rahmani";
+var fullName = firstName + " " + lastName;
+
+function multiply(){
+	if(13 % 3 === 0){
+		return "13 is a multiplier of 3";
+	}
+	return "it's not";
+}
+
+function averageAge() {
+	var array = [13,14,13,15,16,17,19,13,16,15];
+	var total = 0;
+	for(var i = 0; i < array.length ; i++){
+		total += array[i];
+	}
+	return total / array.length;
+}
+
+function calculateAge() {
+	var age = 26;
+	var total = 0;
+	total = age * 354 * 24 * 60 * 60 + " " + "seconds";
+	return total;
+}
--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,15 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+
+function isSubset(array1, array2) {
+	for(var i = 0; i < array1.length; i++){
+		var arr1 = array1[i];
+		for(var j = 0; j < array2.length; j++){
+			if(arr1 = array2[j]){
+				return true;
+			}
+			return false;
+		}
+    }
+}
