f84409c5d10c9613e9be8ebdf8112f15390dffc1
insafmechergui <insaf_mechergui@outlook.com>
2019-10-30 10:08:28 +0100

    warmUp 11

---
+++ fileWarmpUp11.js
@@ -0,0 +1,9 @@
+$('#right').on('click', function(){
+	$('#liste2').append('<li>new item to the right</li>');
+
+});
+
+$('#left').on('click', function(){
+	$('#liste1').append('<li>new item to the left </li>');
+
+});
