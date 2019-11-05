91f9b700f4995d5dd3d8c1971933db7df7c441da
insafmechergui <insaf_mechergui@outlook.com>
2019-11-05 09:45:12 +0100

    warm  up 15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,23 @@
 //
 //
 // Note : you can`t use any built-in functions .
+/*
+function reverseStr(string) {
+	var array = string.split(' ');
+	var tab = [];
+	for (var i =0; i < array.length; i++) {
+		tab.unshift(array[i]);
+	}
+	return tab.join(" ");
+}
+
+*/
+function reverseStr(string) {
+	var str="";
+	for (var i = string.length-1; i >= 0; i--) {
+		str+= string[i];
+	}
+	return str;
+}
+
+///
