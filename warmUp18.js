ec9dc5efb7056d0da83c59098c6d5dbb061d6518
insafmechergui <insaf_mechergui@outlook.com>
2019-11-09 09:53:41 +0100

    warm up 18

--- warmUp18.js
+++ warmUp18.js
@@ -1,6 +1,10 @@
+
+
+
 // You are given an input string.
 
-// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+// For each symbol in the string if it's the first character occurence, 
+//replace it with a '1', else replace it with the amount of times you've already seen it...
 
 // But will your code be performant enough?
 // Examples:
@@ -10,3 +14,22 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+//for string.length
+//compare i by i+1
+
+function charOccurrence(string) {
+var str = 0;
+var tab = [];
+	for (var i = 0; i < string.length;i++) {
+		for (var j = 0; j < string.length;j++) {
+			if(string[i] === string[j]){
+
+				str = string[i]+1;
+			console.log(string[i])
+
+
+            }
+}
+    }
+}
